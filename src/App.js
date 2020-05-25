5cad4da774340e8995e47f54bfeaba2f33585ff2
Ayush <asatkar.ayush@gmail.com>
2020-05-26 01:13:04 +0530

    cards data and country selection

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {fetchData } from './api/index';
 class App extends React.Component {
 state = {
   data: {},
+  country: '',
 }
 
 
@@ -17,15 +18,22 @@ state = {
 
     this.setState({data : fetchedData})
   }
+
+  handleCountryChange = async (country) => {
+    const fetchedData = await fetchData(country);
+
+    this.setState({data : fetchedData, country:country})
+    
+  }
   render(){
 
-    const { data } = this.state; 
+    const { data , country } = this.state; 
 
     return(
       <div className={styles.container}>
         <Cards data={data} />
-        <CountryPicker />
-        <Chart />
+        <CountryPicker handleCountryChange={this.handleCountryChange}/>
+        <Chart data={data} country={country}/>
       </div>
     )
   }
